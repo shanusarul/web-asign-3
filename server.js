e3528b99ead58b81fcc7b35bedaed21d9fb7af38
shanusarul <shanusarul4@gmail.com>
2025-03-10 16:54:33 -0400

    new commit

--- server.js
+++ server.js
@@ -1,93 +1,62 @@
 /********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: __shanus arulanantham____________________ Student ID: _146804232_____________ Date: ___18.02.2025___________
+*  Name: __shanus arulanantham____________________ Student ID: _146804232_____________ Date: ___10.03.2025___________
 *
-*  Published (web app) URL: 
+*  Published URL: 
 *
 ********************************************************************************/
 
 const siteData = require("./modules/data-service");
+const path = require('path');
 
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
 
 const HTTP_PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/views/home.html"));
-app.get("/about", (req, res) => res.sendFile(__dirname + "/views/about.html"));
-
-app.get("/sites", async (req,res)=>{
-  let sites = await siteData.getAllSites();
-  res.send(sites);
-});
+// Updated routes to use res.render instead of res.sendFile
+app.get("/", (req, res) => res.render("home"));
+app.get("/about", (req, res) => res.render("about", {page: '/about'}));
 
 app.get('/sites/:siteId', async (req, res) => {
   try {
-      const site = await dataService.getSiteById(req.params.siteId);
-      if (site) {
-          res.json(site);
-      } else {
-          res.status(404).json({ error: 'Site not found' });
-      }
+    const site = await siteData.getSiteById(req.params.siteId);
+    res.render("site", {site: site, page: ''});
   } catch (error) {
-      res.status(404).json({ error: 'Error' });
+    res.status(404).render("404", {message: error, page: ''});
   }
 });
-
 
 app.get('/sites', async (req, res) => {
   try {
-      if (req.query.region) {
-          const sites = await dataService.getSitesByRegion(req.query.region);
-          return res.json(sites);
-      } else if (req.query.provinceOrTerritory) {
-          const sites = await dataService.getSitesBySubRegionName(req.query.provinceOrTerritory);
-          return res.json(sites);
-      } else {
-          const allSites = await dataService.getAllSites();
-          return res.json(allSites);
-      }
+    if (req.query.region) {
+      const sites = await siteData.getSitesByRegion(req.query.region);
+      res.render("sites", {sites: sites, page: '/sites'});
+    } else if (req.query.provinceOrTerritory) {
+      const sites = await siteData.getSitesByProvinceOrTerritoryName(req.query.provinceOrTerritory);
+      res.render("sites", {sites: sites, page: '/sites'});
+    } else {
+      const allSites = await siteData.getAllSites();
+      res.render("sites", {sites: allSites, page: '/sites'});
+    }
   } catch (error) {
-      res.status(404).json({ error: 'Sites not found' });
+    res.status(404).render("404", {message: error, page: ''});
   }
 });
 
-app.get("/sites/site-id-demo", async (req,res)=>{
-  try{
-    let site = await siteData.getSiteById("ON016");
-    res.send(site);
-  }catch(err){
-    res.send(err);
-  }
-});
+// Handle 404 errors
+app.use((req, res) => res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for", page: ''}));
 
-app.get("/sites/region-demo", async (req,res)=>{
-  try{
-    let sites = await siteData.getSitesByRegion("Prairie Provinces");
-    res.send(sites);
-  }catch(err){
-    res.send(err);
-  }
-});
-
-app.get("/sites/province-or-territory-demo", async (req,res)=>{
-  try{
-    let sites = await siteData.getSitesByProvinceOrTerritoryName("Ontario");
-    res.send(sites);
-  }catch(err){
-    res.send(err);
-  }
-});
-
-app.use((req, res) => res.status(404).sendFile(__dirname + "/views/404.html"));
-
-siteData.initialize().then(()=>{
+siteData.initialize().then(() => {
   app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
 });
